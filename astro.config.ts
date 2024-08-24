b7a5db0f77da46742fbe47821aa854295a91aafc
Florentin Sakwiset <florentin@wemake.cx>
2024-08-25 00:11:50 +0200

    Add @astrojs/cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -1,29 +1,28 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import UnoCSS from 'unocss/astro'
-import vue from '@astrojs/vue'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro';
+import vue from '@astrojs/vue';
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://astro-theme-vitesse.netlify.app/',
   server: {
-    port: 1977,
+    port: 1977
   },
-  integrations: [
-    mdx(),
-    sitemap(),
-    UnoCSS({
-      injectReset: true,
-    }),
-    vue(),
-  ],
+  integrations: [mdx(), sitemap(), UnoCSS({
+    injectReset: true
+  }), vue()],
   markdown: {
     shikiConfig: {
       themes: {
         light: 'github-light-default',
-        dark: 'github-dark-default',
+        dark: 'github-dark-default'
       },
-      wrap: true,
-    },
+      wrap: true
+    }
   },
-})
+  output: "server",
+  adapter: cloudflare()
+});
