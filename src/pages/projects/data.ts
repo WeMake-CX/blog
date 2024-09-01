f04d3940d1accac3fd3306e9a34ea6baee9e78de
Florentin Sakwiset <florentin@wemake.cx>
2024-09-01 18:15:27 +0200

    ✨ (src/pages/projects/data.ts): Add project data to showcase WeMake's work
    This commit populates the project data with details about WeMake's projects, including descriptions, icons, and links. This provides website visitors with a clear overview of WeMake's capabilities and areas of expertise.

--- src/pages/projects/data.ts
+++ src/pages/projects/data.ts
@@ -7,12 +7,13 @@ export const projectData: ProjectData = [
       {
         text: '💙 WeMake',
         description:
-          'WeMake ist ein zukunftsorientiertes Team, das fortschrittliche Technologien und umfassende Branchenkenntnisse einsetzt, um die digitale Transformation von Unternehmen zu erleichtern.',
+          'Wir nutzen neueste Technologien und unser Know-how, um Unternehmen bei der digitalen Transformation zu unterstützen.',
         icon: 'i-carbon-campsite',
         href: 'https://wemake.cx',
       },
       {
         text: 'Stealth Startup',
+        description: 'Top Secret Projekt - mehr wird noch nicht verraten!',
         icon: 'i-carbon-campsite',
         href: '/',
       },
@@ -20,6 +21,56 @@ export const projectData: ProjectData = [
   },
   {
     title: 'Projects',
-    projects: [],
+    projects: [
+      {
+        text: 'V41 Platform',
+        description:
+          'KI-Power für deine Produktivität: Aufgaben automatisieren, Prioritäten setzen und Ziele effizient erreichen.',
+        icon: 'i-carbon-campsite',
+        href: '/',
+      },
+      {
+        text: 'WeMake Calls',
+        description:
+          'Videokonferenzen leicht gemacht: Echtzeitkommunikation, Bildschirmfreigabe & Benutzerverwaltung für globale Teams.',
+        icon: 'i-carbon-campsite',
+        href: 'https://calls.wemake.cx',
+      },
+      {
+        text: 'CX Blog',
+        description:
+          'Einblicke & Geschichten aus der Creator Experience Community -  lass dich inspirieren!',
+        icon: 'i-carbon-campsite',
+        href: 'https://github.com/WeMake-CX/blog',
+      },
+      {
+        text: 'Intelligent Content Understanding',
+        description:
+          'ICU: LLMs verstehen Inhalte tiefgründig & fördern kreative Lösungen durch vernetztes Wissen.',
+        icon: 'i-carbon-campsite',
+        href: 'https://huggingface.co/datasets/WeMake/Intelligent-Content-Understanding',
+      },
+      {
+        text: 'Llama-3-8B-Instruct-V41-1048k',
+        description:
+          'Unser fortschrittliches Sprachmodell: Einfühlsame Interaktionen & ethische KI-Anwendungen.',
+        icon: 'i-carbon-campsite',
+        href: 'https://huggingface.co/WeMake/Llama-3-8B-Instruct-V41-1048k',
+      },
+      {
+        text: 'LUNA-ProACT',
+        description:
+          'Echtzeitschutz vor Online-Bedrohungen & Cybergrooming-Prävention auf deinem Handy.',
+        icon: 'i-carbon-campsite',
+        href: 'https://huggingface.co/WeMake/LUNA-ProACT',
+      },
+      {
+        text: 'VX-Unholy-13B',
+        description:
+          'Open-Source-LLM für mehr Einfühlungsvermögen & ethische KI-Entwicklung. Sei dabei!',
+        icon: 'i-carbon-campsite',
+        href: 'https://huggingface.co/WeMake/VX-Unholy-13B',
+      },
+    ],
   },
 ]
